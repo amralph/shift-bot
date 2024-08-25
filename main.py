0e32cdb650bafc7ae0cfa7125ee365dd64f4825f
RalphSallan <rs1917@hotmail.com>
2024-08-25 02:38:00 -0400

    remove logged_in variable
    add log_in function documentation
    
    update log_in function error handling message
    
    update main loop (create and delete driver everytime, for easier logic).

--- main.py
+++ main.py
@@ -14,20 +14,31 @@ USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
 WEBSITE = os.getenv('WEBSITE')
 
-REFRESH_INTERVAL = 100
-logged_in = False
+REFRESH_INTERVAL = 5
 
 
 def log_in(user, password, driver):
-    global logged_in
+    """Logs in user in the browser given user, password, and driver
 
+    Parameters:
+    user (string): user id
+
+    password (string): password
+
+    driver (WebDriver): an instance of `selenium.webdriver.chrome.webdriver.WebDriver`
+
+    Returns: none
+
+    Raises:
+    NoSuchElementException: if elements can't be found by the driver
+    """
     try:
-        # find elements, if not found, raise exception (the code will break if you don't do this)
+        # find elements, and if not found, raise exception (the code will break if you don't do this)
         username_box = driver.find_element(By.ID, 'txtUserName')
         password_box = driver.find_element(By.ID, 'txtPassword')
         sign_in_button = driver.find_element(By.ID, 'cmdLogin')
 
-        # clear boxes (good practice)
+        # clear boxes before typing (good practice)
         username_box.clear()
         password_box.clear()
 
@@ -38,23 +49,28 @@ def log_in(user, password, driver):
         # press log in button
         sign_in_button.click()
 
-        # need to verify that user is actually logged in!!!
-
-        # set logged_in to true
-        logged_in = True
-
-    except NoSuchElementException:
-        print("Element not found")
+    except NoSuchElementException as e:
+        print(e)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    DRIVER = webdriver.Chrome()
-    DRIVER.get(WEBSITE)
-
-    if not logged_in:
-        log_in(USER, PASSWORD, DRIVER)
 
     while True:
+        # initialize driver
+        DRIVER = webdriver.Chrome()
+        DRIVER.get(WEBSITE)
+
+        print(type(DRIVER))
+
+        # log in
+        log_in(USER, PASSWORD, DRIVER)
+
+        # do stuff
+        time.sleep(5)
+
+        # kill driver (logging out is unnecessary with this line
+        DRIVER.quit()
+
         # run the loop every REFRESH_INTERVAL
         time.sleep(REFRESH_INTERVAL)
